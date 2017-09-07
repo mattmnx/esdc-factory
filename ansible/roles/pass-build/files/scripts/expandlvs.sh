9101a66a16973e8e885504c739d04e20f2d207b6
Matthew Armstrong <matthewarmstrong@Matthews-MacBook-Pro.local>
2017-09-07 16:38:57 -0400

    added lv resize script

---
+++ ansible/roles/pass-build/files/scripts/expandlvs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sectorend=$(fdisk /dev/vda -l |grep "Disk /dev/vda" | cut -d" " -f 7)
+current=$(fdisk /dev/vda -l |tail -1 |cut -d" " -f 9)
+difference=`expr $sectorend - $current`
+if [ $difference -ne 1 ]; then
+    echo -e "n\np\n\n\n\nw\nq\n" | fdisk /dev/vda
+    reboot
+fi
+IFS=$'\n'
+for partition in `fdisk /dev/vda -l |grep "Linux$"`; do
+        addtolv=$(echo $partition | cut -d" " -f1)
+        pvcreate $addtolv
+        vgextend rootvg $addtolv
+        lvresize --resizefs -l +100%FREE /dev/mapper/rootvg-rootvg
+done
