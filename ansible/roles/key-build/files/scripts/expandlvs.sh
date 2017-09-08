b532f0f961d0b41e485f012431de79a3a7d9538a
Matthew Armstrong <matthewarmstrong@Matthews-MacBook-Pro.local>
2017-09-08 11:13:09 -0400

    Updated lvextend script to avoid reboot

--- ansible/roles/key-build/files/scripts/expandlvs.sh
+++ ansible/roles/key-build/files/scripts/expandlvs.sh
@@ -3,8 +3,8 @@ sectorend=$(fdisk /dev/vda -l |grep "Disk /dev/vda" | cut -d" " -f 7)
 current=$(fdisk /dev/vda -l |tail -1 |cut -d" " -f 9)
 difference=`expr $sectorend - $current`
 if [ $difference -ne 1 ]; then
-    echo -e "n\np\n\n\n\nw\nq\n" | fdisk /dev/vda
-    reboot
+    echo -e "n\ne\n\n\n\nw\nq\n" | fdisk /dev/vda
+    partprobe
 fi
 IFS=$'\n'
 for partition in `fdisk /dev/vda -l |grep "Linux$"`; do
@@ -12,4 +12,6 @@ for partition in `fdisk /dev/vda -l |grep "Linux$"`; do
         pvcreate $addtolv
         vgextend rootvg $addtolv
         lvresize --resizefs -l +100%FREE /dev/mapper/rootvg-rootlv
+        partnum=${addtolv: -1}
+        echo -e "t\n$partnum\n8e\nw\n" | fdisk /dev/vda
 done
