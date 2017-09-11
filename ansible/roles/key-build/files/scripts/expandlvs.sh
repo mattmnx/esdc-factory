7da9bd0c01920a499253b525924d84d9a9401903
Matthew Armstrong <matthewarmstrong@Matthews-MacBook-Pro.local>
2017-09-11 13:24:13 -0400

    Updated to pvextend and also changed iso selection

--- ansible/roles/key-build/files/scripts/expandlvs.sh
+++ ansible/roles/key-build/files/scripts/expandlvs.sh
@@ -2,16 +2,8 @@
 sectorend=$(fdisk /dev/vda -l |grep "Disk /dev/vda" | cut -d" " -f 7)
 current=$(fdisk /dev/vda -l |tail -1 |cut -d" " -f 9)
 difference=`expr $sectorend - $current`
-if [ $difference -ne 1 ]; then
-    echo -e "n\np\n\n\n\nw\nq\n" | fdisk /dev/vda
-    partprobe
+if [ $difference -gt 100 ]; then
+    growpart /dev/vda 1
+    pvresize /dev/vda1
+    lvresize --resizefs -l +100%FREE /dev/mapper/rootvg-rootlv
 fi
-IFS=$'\n'
-for partition in `fdisk /dev/vda -l |grep "Linux$"`; do
-        addtolv=$(echo $partition | cut -d" " -f1)
-        pvcreate $addtolv
-        vgextend rootvg $addtolv
-        lvresize --resizefs -l +100%FREE /dev/mapper/rootvg-rootlv
-        partnum=${addtolv: -1}
-        echo -e "t\n$partnum\n8e\nw\n" | fdisk /dev/vda
-done
