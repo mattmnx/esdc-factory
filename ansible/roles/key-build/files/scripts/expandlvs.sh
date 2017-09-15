e82a01c0e90426539d1c9b23be7d58baad82ee9e
Matthew Armstrong <matthewarmstrong@Matthews-MacBook-Pro.local>
2017-09-15 10:42:48 -0400

    removing script growpart and moving to cloud init

--- ansible/roles/key-build/files/scripts/expandlvs.sh
+++ ansible/roles/key-build/files/scripts/expandlvs.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-sectorend=$(fdisk /dev/vda -l |grep "Disk /dev/vda" | cut -d" " -f 7)
-current=$(fdisk /dev/vda -l |tail -1 |cut -d" " -f 9)
-difference=`expr $sectorend - $current`
-if [ $difference -gt 100 ]; then
-    growpart /dev/vda 1
-    pvresize /dev/vda1
-    lvresize --resizefs -l +100%FREE /dev/mapper/rootvg-rootlv
-fi
+# sectorend=$(fdisk /dev/vda -l |grep "Disk /dev/vda" | cut -d" " -f 7)
+# current=$(fdisk /dev/vda -l |tail -1 |cut -d" " -f 9)
+# difference=`expr $sectorend - $current`
+# if [ $difference -gt 100 ]; then
+#     growpart /dev/vda 1
+#     pvresize /dev/vda1
+#     lvresize --resizefs -l +100%FREE /dev/mapper/rootvg-rootlv
+# fi
+
+pvresize /dev/vda1
+lvresize --resizefs -l +100%FREE /dev/mapper/rootvg-rootlv
